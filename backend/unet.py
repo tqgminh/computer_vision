1e4633a73d3624ac5d5fb04152bcf02cd55c3780
Tran Minh <quangminhtran22@gmail.com>
2021-12-11 17:13:12 +0700

    commit

--- backend/unet.py
+++ backend/unet.py
@@ -18,8 +18,7 @@ def unet(src_path, dst_path):
     mask = model.predict(np.reshape(img, (1, 128, 128, 3)))
     mask = np.argmax(mask, axis=3)
     mask = mask[0]
-    # plt.imshow(mask)
-    # plt.show()
+
     res = np.zeros((128, 128, 3))
     for h in range(mask.shape[0]):
         for w in range(mask.shape[1]):
@@ -27,21 +26,27 @@ def unet(src_path, dst_path):
             if mask[h, w] == 1:
                 res[h, w, 0] = 0
                 res[h, w, 1] = 0
+                res[h, w, 2] = 255
+            elif mask[h, w] == 2:
+                res[h, w, 0] = 0
+                res[h, w, 1] = 0
                 res[h, w, 2] = 0
             else:
                 res[h, w, 0] = 255
-                res[h, w, 1] = 255
-                res[h, w, 2] = 255
+                res[h, w, 1] = 0
+                res[h, w, 2] = 0
 
     # res = res.astype(np.int8)
 
     labeled_img = cv2.resize(res, (W, H))
 
+    kernel = np.array([[-1, -1, -1],
+                       [-1, 9, -1],
+                       [-1, -1, -1]])
+    labeled_img = cv2.filter2D(src=labeled_img, ddepth=-1, kernel=kernel)
     cv2.imwrite(dst_path, labeled_img)
-
-    return mask
 
 
 if __name__ == '__main__':
 
-    unet('Abyssinian_31.jpg', 'unet/res.jpg')
+    unet('images/staffordshire_bull_terrier_102.jpg', 'unet/res.jpg')
